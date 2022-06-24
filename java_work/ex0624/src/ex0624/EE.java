49c8f7382fc94e29b7621ef740873f7867341756
Hyunju-Cho <hjucho5616@gmail.com>
2022-06-24 11:54:10 +0900

    ex0624

---
+++ java_work/ex0624/src/ex0624/EE.java
@@ -0,0 +1,17 @@
+package ex0624;
+
+public class EE {
+
+	public static void main(String[] args) {
+
+		double pi=3.14;
+		int pia=(int) pi;//ctrl+1번
+		
+		System.out.println("pia="+pia);
+		
+		double a=3.12121212;
+		System.out.println("(int)a="+(int)a);
+		
+	}
+
+}
