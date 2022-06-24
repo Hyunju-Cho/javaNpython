5878fa3db23274a0fc88e180f1781ed92f18e1e9
Hyunju-Cho <hjucho5616@gmail.com>
2022-06-24 11:04:56 +0900

    ex0624

---
+++ java_work/ex0624/src/ex0624/CC.java
@@ -0,0 +1,20 @@
+package ex0624;
+
+public class CC {
+
+	public static void main(String[] args) {
+
+		short a=10;
+		short b=20;
+		
+		System.out.println("c="+(a+b));//int형 연산
+		
+		//short c= a+b;//오류
+		//System.out.println("c="+c);
+		
+		
+		
+		
+	}
+
+}
