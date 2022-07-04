2e80d13c50e706e20c6a621ed61d2652e9f959a5
Hyunju-Cho <hjucho5616@gmail.com>
2022-07-04 11:49:31 +0900

    20220704

---
+++ java_work/ex0704/src/ex0704/AA.java
@@ -0,0 +1,18 @@
+package ex0704;
+
+public class AA {
+	
+	/*선언*/
+
+	public static int a=10;
+	
+	public void doA() {
+		a+=10;
+		System.out.println("doa a = "+a);
+	}
+	
+	public void doB() {
+		a+=20;
+		System.out.println("dob a = "+a);
+	}
+}
---
+++ python_work/220704/ex01.py
@@ -0,0 +1,12 @@
+from aa import *
+
+aa=AA()
+aa.doA()
+aa.doB()
+aa.doC()
+
+aaa=AA()
+
+aaa.doA()
+aaa.doB()
+aaa.doC()
