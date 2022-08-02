eeb79b65262cc69da16385e66bf9ad85b37c78c7
Hyunju-Cho <hjucho5616@gmail.com>
2022-08-02 14:45:12 +0900

    220802

---
+++ flask_work/ex01.py
@@ -0,0 +1,75 @@
+from turtle import end_fill
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+import time
+
+from sklearn.neighbors import KNeighborsClassifier
+
+##########데이터 로드
+x_data = np.array([
+    #O
+    [0, 255, 255, 255, 0,
+     255, 0, 0, 0, 255,
+     255, 0, 0, 0, 255,
+     255, 0, 0, 0, 255,
+     0, 255, 255, 255, 0],
+    [0, 255, 255, 255, 0,
+     255, 255, 0, 0, 255,
+     255, 0, 0, 0, 255,
+     255, 0, 0, 0, 255,
+     0, 255, 255, 255, 0],
+    [0, 255, 255, 255, 0,
+     255, 0, 0, 255, 255,
+     255, 0, 0, 0, 255,
+     255, 0, 0, 0, 255,
+     0, 255, 255, 255, 0],
+
+    #X
+    [255, 0, 0, 0, 255,
+     0, 255, 0, 255, 0,
+     0, 0, 255, 0, 0,
+     0, 255, 0, 255, 0,
+     255, 0, 0, 0, 255],
+    [255, 255, 0, 0, 255,
+     0, 255, 0, 255, 0,
+     0, 0, 255, 0, 0,
+     0, 255, 0, 255, 0,
+     255, 0, 0, 0, 255],
+    [255, 0, 0, 255, 255,
+     0, 255, 0, 255, 0,
+     0, 0, 255, 0, 0,
+     0, 255, 0, 255, 0,
+     255, 0, 0, 0, 255]
+])
+y_data = np.array([
+    [0],
+    [0],
+    [0],
+    [1],
+    [1],
+    [1]
+])
+
+y_data=['O']*3+['X']*3
+labels = ['오', '엑스']
+
+start_time=time.time()
+knclf=KNeighborsClassifier(n_neighbors=2)
+knclf.fit(x_data,y_data)
+end_time=time.time()
+
+print('걸린시간 = ',end_time-start_time)
+
+pred=knclf.predict(x_data[0].reshape(-1,25))
+print(pred)
+
+# for i in range(6):
+#     plt.title(f'target={y_data[i]}')
+#     plt.imshow(x_data[i].reshape(5,5),cmap='gray_r')
+#     plt.savefig(f'x_data[{i}]_plt.png')
+#     #plt.show()
+#     plt.close()
+
+# for i in range(6):
+#     cv2.imwrite(f'x_data[{i}]_cv.png',x_data[i].reshape(5,5))
